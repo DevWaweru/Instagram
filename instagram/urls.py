36f9f28d83761f4ea93d263ed66ae983487f4f9e
Richard Waweru <developer.waweru@gmail.com>
2018-06-15 15:04:01 +0300

    AUTHENTICATION  - Signup module  - Generate activation token for signing up  - Send activation email after sign up  - Login module

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('gram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    
+    url(r'^logout/$',views.logout, {"next_page": '/'})
 ]
