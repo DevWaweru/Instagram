fe1dbf5f1087db136269f93bb0aee282d83788f7
Richard Waweru <developer.waweru@gmail.com>
2018-06-21 11:33:13 +0300

    Adding Comments in admin panel

--- gram/admin.py
+++ gram/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Image
+from .models import Profile, Image,Comments
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Image)
+admin.site.register(Comments)
